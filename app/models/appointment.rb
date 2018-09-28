0dfa160138ba19d61a5a90251606bf3d9f63f139
Ariel Zhen <zhenpingping@live.com>
2018-09-28 07:04:03 +0000

    automatically backed up by learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,7 +2,8 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
   belongs_to :patient
 
-  def appointment_datetime_to_s 
+  def appointment_datetime_to_s
     self.appointment_datetime.strftime("%B %e,%Y at %I:%M%p")
-  end 
+  end
+  
 end
